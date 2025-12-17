9ec0311935bdb6d480fb05f8bdcf41bd360cf969
AbhayThakre <abhaythakre532@gmail.com>
2025-12-17 22:39:44 +0530

    setup everything done

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-dotenv.config();
+dotenv.config(); // load .env
 
 const app = express();
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+
+// Connect Database
+connectDB();
+
+
+
+
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
+
+
 // Test route
 app.get("/", (req, res) => {
-  res.send("🚀 Backend server is running!");
+  res.send("🚀 Backend + MongoDB server is running!");
 });
 
-// Port
-const PORT = process.env.PORT || 5000;
 
-// Start server
+
+
+
+
+
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
